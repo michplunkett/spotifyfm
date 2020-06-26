8e157f69f45428cc49fd0b619d7a31a215f5e6d4
Michael Plunkett <michael@blacklocus.com>
2020-06-26 16:10:41 -0500

    initial checkin

---
+++ utility/constants.go
@@ -0,0 +1,5 @@
+package utility
+
+const (
+	EmptyString string = ""
+)
---
+++ utility/helperfunctions.go
@@ -0,0 +1,16 @@
+package utility
+
+
+type HelperFunctions interface {
+	ArrayHasNoEmptyStrings(envVars []string) bool
+}
+
+func ArrayHasNoEmptyStrings(envVars []string) bool {
+	for _, value := range envVars {
+		if value == EmptyString {
+			return true
+		}
+	}
+
+	return false
+}
