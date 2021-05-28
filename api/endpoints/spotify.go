fbaaf29ce3edd03b37893e56c75a2a048a302c17
michplunkett <michplunkett@gmail.com>
2021-05-28 10:35:08 -0500

    Sorting functions

--- api/endpoints/spotify.go
+++ api/endpoints/spotify.go
@@ -9,11 +9,11 @@ const (
 )
 
 type SpotifyHandler interface {
-	GetUserInfo() *spotify.PrivateUser
-	GetCurrentlyPlaying() *spotify.CurrentlyPlaying
-	GetAllTopTracks(timeRange string) []spotify.FullTrack
 	GetAllTopArtists(timeRange string) []spotify.FullArtist
+	GetAllTopTracks(timeRange string) []spotify.FullTrack
 	GetAudioFeaturesOfTrack(trackIDs []spotify.ID) []*spotify.AudioFeatures
+	GetCurrentlyPlaying() *spotify.CurrentlyPlaying
+	GetUserInfo() *spotify.PrivateUser
 	SearchForSong(songName, albumName, artistName string) ([]spotify.FullTrack, error)
 }
 
@@ -27,23 +27,9 @@ func NewSpotifyHandler(client *spotify.Client) *spotifyHandler {
 	}
 }
 
-func (handler *spotifyHandler) GetUserInfo() *spotify.PrivateUser {
-	userInfo, _ := handler.client.CurrentUser()
-	return userInfo
-}
-
-func (handler *spotifyHandler) GetCurrentlyPlaying() *spotify.CurrentlyPlaying {
-	currentlyPlaying, _ := handler.client.PlayerCurrentlyPlaying()
-	return currentlyPlaying
-}
-
-func (handler *spotifyHandler) getTopTracks(timeRange string, offset, pageSize int) []spotify.FullTrack {
-	topTracks, _ := handler.client.CurrentUsersTopTracksOpt(&spotify.Options{
-		Limit:     &pageSize,
-		Offset:    &offset,
-		Timerange: &timeRange,
-	})
-	return topTracks.Tracks
+func (handler *spotifyHandler) GetAllTopArtists(timeRange string) []spotify.FullArtist {
+	// There currently isn't an offset option for CurrentUsersTopArtistsOpt so I'm doing one large grab
+	return handler.getTopArtists(timeRange, pageSizeConst)
 }
 
 func (handler *spotifyHandler) GetAllTopTracks(timeRange string) []spotify.FullTrack {
@@ -62,6 +48,24 @@ func (handler *spotifyHandler) GetAllTopTracks(timeRange string) []spotify.FullT
 	return tracks
 }
 
+func (handler *spotifyHandler) GetAudioFeaturesOfTrack(trackIDs []spotify.ID) []*spotify.AudioFeatures {
+	audioFeatures, _ := handler.client.GetAudioFeatures(trackIDs...)
+	if len(audioFeatures) > 0 {
+		return audioFeatures
+	}
+	return nil
+}
+
+func (handler *spotifyHandler) GetCurrentlyPlaying() *spotify.CurrentlyPlaying {
+	currentlyPlaying, _ := handler.client.PlayerCurrentlyPlaying()
+	return currentlyPlaying
+}
+
+func (handler *spotifyHandler) GetUserInfo() *spotify.PrivateUser {
+	userInfo, _ := handler.client.CurrentUser()
+	return userInfo
+}
+
 func (handler *spotifyHandler) getTopArtists(timeRange string, pageSize int) []spotify.FullArtist {
 	topArtists, _ := handler.client.CurrentUsersTopArtistsOpt(&spotify.Options{
 		Limit:     &pageSize,
@@ -70,17 +74,13 @@ func (handler *spotifyHandler) getTopArtists(timeRange string, pageSize int) []s
 	return topArtists.Artists
 }
 
-func (handler *spotifyHandler) GetAllTopArtists(timeRange string) []spotify.FullArtist {
-	// There currently isn't an offset option for CurrentUsersTopArtistsOpt so I'm doing one large grab
-	return handler.getTopArtists(timeRange, pageSizeConst)
-}
-
-func (handler *spotifyHandler) GetAudioFeaturesOfTrack(trackIDs []spotify.ID) []*spotify.AudioFeatures {
-	audioFeatures, _ := handler.client.GetAudioFeatures(trackIDs...)
-	if len(audioFeatures) > 0 {
-		return audioFeatures
-	}
-	return nil
+func (handler *spotifyHandler) getTopTracks(timeRange string, offset, pageSize int) []spotify.FullTrack {
+	topTracks, _ := handler.client.CurrentUsersTopTracksOpt(&spotify.Options{
+		Limit:     &pageSize,
+		Offset:    &offset,
+		Timerange: &timeRange,
+	})
+	return topTracks.Tracks
 }
 
 func (handler *spotifyHandler) SearchForSong(artistName, albumName, songName string) ([]spotify.FullTrack, error) {
